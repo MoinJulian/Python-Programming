13d6d75194e1a42cb6ddb7c7a03241e3977b7c1b
Julian Hammer <jh@moinjulian.com>
2024-01-08 15:18:20 +0000

    Changed import statements to use the full module name.

--- own/turtle/circle.py
+++ own/turtle/circle.py
@@ -1,20 +1,20 @@
 import turtle
 import colorsys
 
-bgcolor("black")
-pensize(3)
-tracer(5)
-setpos(-90, 80)
-hideturtle()
+turtle.bgcolor("black")
+turtle.pensize(3)
+turtle.tracer(5)
+turtle.setpos(-90, 80)
+turtle.hideturtle()
 
 h = 0.0
 
 for i in range(400):
     c = colorsys.hsv_to_rgb(h, 1, 1)
-    pencolor(c)
-    fd(200)
-    rt(91)
-    circle(60)
+    turtle.pencolor(c)
+    turtle.fd(200)
+    turtle.rt(91)
+    turtle.circle(60)
     h += 0.009
 
-done()
+turtle.done()
